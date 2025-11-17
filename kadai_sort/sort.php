ee93bb3c8e35cfbcba781a9bf86b594edf78b99f
Chi9ron <86711177+Chi9ron@users.noreply.github.com>
2025-11-17 22:39:00 +0900

    kadai_sortをアップロード

---
+++ kadai_sort/sort.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <title>PHP基礎編</title>
+</head>
+
+<body>
+    <p>
+        <?php
+        /**
+         * 配列を昇順または降順にソートし、結果を1行ずつ出力する関数
+         *
+         * @param array $array ソート対象の配列
+         * @param bool $is_ascending TRUE: 昇順, FALSE: 降順
+         */
+        function sort_2way($array, $is_ascending = TRUE) {
+            if ($is_ascending) {
+                sort($array);
+                echo "昇順にソートします。<br>";
+            } else {
+                rsort($array);
+                echo "降順にソートします。<br>";
+            }
+
+            foreach ($array as $element) {
+                echo $element . "<br>";
+            }
+        }
+
+        $nums = [15, 4, 18, 23, 10];
+
+        sort_2way($nums, TRUE);
+
+        sort_2way($nums, FALSE);
+        ?>
+    </p>
+</body>
+
+</html>
